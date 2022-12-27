871aeafcc9738e19ed5f6a61aea498033efb8f6e
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-12-27 18:05:03 +0100

    breaking: changed export

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,12 @@ import { retrieveConfig } from "./configutations/plugin";
 import { UserOptions } from "./interfaces/UserOptions";
 import { compileTs } from "./lib/TypeScriptCompiler";
 
-const unplugin = createUnplugin((options: UserOptions) => {
+export const NativeFederationTypeScriptRemote = createUnplugin((options: UserOptions) => {
   const { tsConfig, mapComponentsToExpose } = retrieveConfig(options)
   return {
-    name: 'native-federation-typescript',
+    name: 'native-federation-typescript/remote',
     writeBundle() {
       compileTs(mapComponentsToExpose, tsConfig)
     }
   }
 })
-
-export default unplugin
