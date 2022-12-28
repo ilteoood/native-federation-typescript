d9086bddf555097877b138e430f8ff45d1937b11
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-12-28 15:37:43 +0100

    fix: vite

--- src/vite.ts
+++ src/vite.ts
@@ -1,4 +1,6 @@
 import {NativeFederationTypeScriptHost as GeneralHost, NativeFederationTypeScriptRemote as GeneralRemote} from '.'
+import {HostOptions} from './interfaces/HostOptions'
+import {RemoteOptions} from './interfaces/RemoteOptions'
 
-export const NativeFederationTypeScriptRemote = GeneralRemote.vite
-export const NativeFederationTypeScriptHost = GeneralHost.vite
+export const NativeFederationTypeScriptRemote = GeneralRemote.vite as (options: RemoteOptions) => any
+export const NativeFederationTypeScriptHost = GeneralHost.vite as (options: HostOptions) => any
