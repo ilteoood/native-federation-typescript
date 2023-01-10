55476942e8545d9933da6e48bc4710d9a7b0f6af
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-01-10 21:28:21 +0100

    feat: additional tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,23 +1,18 @@
+import AdmZip from 'adm-zip'
+import axios from 'axios'
 import dirTree from 'directory-tree'
+import {rm} from 'fs/promises'
 import {join} from 'path'
 import {UnpluginOptions} from 'unplugin'
-import {describe, expect, it} from 'vitest'
+import {describe, expect, it, vi} from 'vitest'
 
-import {NativeFederationTypeScriptRemote} from './index'
+import {NativeFederationTypeScriptHost, NativeFederationTypeScriptRemote} from './index'
 
 describe('index', () => {
     describe('NativeFederationTypeScriptRemote', () => {
         it('throws for missing moduleFederationConfig', () => {
-            const options = {
-                tsConfigPath: './tsconfig.json',
-                typesFolder: '@mf-types',
-                compiledTypesFolder: 'compiled-types',
-                deleteTypesFolder: true,
-                additionalFilesToCompile: []
-            }
-
             // @ts-expect-error missing moduleFederationConfig
-            const writeBundle = () => NativeFederationTypeScriptRemote.rollup(options)
+            const writeBundle = () => NativeFederationTypeScriptRemote.rollup({})
             expect(writeBundle).toThrowError('moduleFederationConfig is required')
         })
 
@@ -55,43 +50,105 @@ describe('index', () => {
                             {
                                 name: 'configurations',
                                 children: [
-                                    {
-                                        name: 'hostPlugin.d.ts'
-                                    },
-                                    {
-                                        name: 'remotePlugin.d.ts'
-                                    }
-                                ]
+                                    {name: 'hostPlugin.d.ts'},
+                                    {name: 'remotePlugin.d.ts'},
+                                ],
                             },
-                            {
-                                name: 'index.d.ts'
-                            },
+                            {name: 'index.d.ts'},
                             {
                                 name: 'interfaces',
                                 children: [
-                                    {
-                                        name: 'HostOptions.d.ts'
-                                    },
-                                    {
-                                        name: 'RemoteOptions.d.ts'
-                                    }
-                                ]
+                                    {name: 'HostOptions.d.ts'},
+                                    {name: 'RemoteOptions.d.ts'},
+                                ],
                             },
                             {
                                 name: 'lib',
                                 children: [
+                                    {name: 'archiveHandler.d.ts'},
+                                    {name: 'typeScriptCompiler.d.ts'},
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            })
+        })
+    })
+
+    describe('NativeFederationTypeScriptHost', () => {
+        it('throws for missing moduleFederationConfig', () => {
+            // @ts-expect-error missing moduleFederationConfig
+            const writeBundle = () => NativeFederationTypeScriptHost.rollup({})
+            expect(writeBundle).toThrowError('moduleFederationConfig is required')
+        })
+
+        it('correctly writeBundle', async () => {
+            const options = {
+                moduleFederationConfig: {
+                    name: 'moduleFederationTypescript',
+                    filename: 'remoteEntry.js',
+                    remotes: {
+                        remotes: 'https://foo.it',
+                    },
+                    shared: {
+                        react: {singleton: true, eager: true},
+                        'react-dom': {singleton: true, eager: true},
+                    },
+                },
+                typesFolder: '@mf-types'
+            }
+
+            const distFolder = join('./dist', options.typesFolder)
+            const zip = new AdmZip()
+            await zip.addLocalFolderPromise(distFolder, {})
+
+            axios.get = vi.fn().mockResolvedValueOnce({data: zip.toBuffer()})
+
+            const unplugin = NativeFederationTypeScriptHost.rollup(options) as UnpluginOptions
+            await expect(unplugin.writeBundle?.()).resolves.not.toThrow()
+
+            expect(dirTree(options.typesFolder)).toMatchObject({
+                name: '@mf-types',
+                children: [
+                    {
+                        name: 'remotes',
+                        children: [
+                            {
+                                name: 'compiled-types',
+                                children: [
                                     {
-                                        name: 'archiveHandler.d.ts'
+                                        name: 'configurations',
+                                        children: [
+                                            {name: 'hostPlugin.d.ts'},
+                                            {name: 'remotePlugin.d.ts'},
+                                        ],
                                     },
                                     {
-                                        name: 'typeScriptCompiler.d.ts'
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
+                                        name: 'index.d.ts',
+                                    },
+                                    {
+                                        name: 'interfaces',
+                                        children: [
+                                            {name: 'HostOptions.d.ts'},
+                                            {name: 'RemoteOptions.d.ts'},
+                                        ],
+                                    },
+                                    {
+                                        name: 'lib',
+                                        children: [
+                                            {name: 'archiveHandler.d.ts'},
+                                            {name: 'typeScriptCompiler.d.ts'},
+                                        ],
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                ],
             })
+
+            await rm(options.typesFolder, {recursive: true, force: true})
         })
     })
 })
